5a6de06a4449cf1404326fa5621321e13ee0363f
Aleksey <oldf03@gmail.com>
2022-12-12 21:40:33 +0300

    Initial Commit

---
+++ ColorsApp/ColorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsAppApp.swift
+//  ColorsApp
+//
+//  Created by Buba on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
