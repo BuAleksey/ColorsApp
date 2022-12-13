c77b4a6bdc289d948cf69238471591197901e58b
Aleksey <oldf03@gmail.com>
2022-12-13 23:06:45 +0300

    final

--- ColorsApp/ContentView.swift
+++ ColorsApp/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redColor = Double.random(in: 1...255)
+    @State private var greenColor = Double.random(in: 1...255)
+    @State private var blueColor = Double.random(in: 1...255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 270, height: 100)
+                .cornerRadius(20)
+                .foregroundColor(Color(
+                    red: redColor/255,
+                    green: greenColor/255,
+                    blue: blueColor/255)
+                )
+            
+            SliderSet(colorText: .red, value: $redColor)
+                .padding(.trailing, 50)
+            SliderSet(colorText: .green, value: $greenColor)
+                .padding(.trailing, 50)
+            SliderSet(colorText: .blue, value: $blueColor)
+                .padding(.trailing, 50)
+            
+            Spacer()
         }
         .padding()
     }
@@ -22,5 +39,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SliderSet: View {
+    let colorText: Color
+    
+    @Binding var value: Double
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .foregroundColor(colorText)
+                .frame(width: 50)
+            
+            Slider(value: $value, in: 1...255, step: 1)
+        }
     }
 }
